a6c422345a78635e11c29da756d9ac88a61f9bd1
Don Strange <dj.strange15@gmail.com>
2022-05-12 23:55:18 +1200

    Nearly finished worksheet 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
 import React from "react";
+import {
+  Route,
+  Link,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
 
-/*import Home from "./pages/Home";
+import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article";
-import NotFoundPage from "./pages/404";*/
+import NotFoundPage from "./pages/404";
 
 const App = () => {
   return (
-    <div>
-      <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-      <ul className="header">
-        <li><a href="/">Home</a></li>
-        <li><a href="/SEPractice">Select the Practice</a></li>
-        <li><a href="/SubmitArticle">Submit an Article</a></li>
-      </ul>
-      <div className="content">
-        
+    <BrowserRouter>
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li><Link exact to="/">Home</Link></li>
+          <li><Link to="/SEPractice">Select the Practice</Link></li>
+          <li><Link to="/SubmitArticle">Submit an Article</Link></li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route path="/SEPractice" element={<SEPractice />}/>
+            <Route path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route exact path="/404" component={<NotFoundPage />}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as ReactDomClient from 'react-dom/client';
 import App from "./App";
+import "./index.css";
 
 //Create a root 
 const root = ReactDomClient.createRoot(
